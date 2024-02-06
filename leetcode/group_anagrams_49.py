e7d38df45a074d43691e20b7b720543218b51183
Venu <34569918+VenuJakkula@users.noreply.github.com>
2024-02-06 23:29:42 +0530

    Create group_anagrams_49.py

---
+++ leetcode/group_anagrams_49.py
@@ -0,0 +1,48 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+
+class Solution:
+    def checkAnagrams(self,str1: str,str2: str) -> bool:
+        if sorted(str1)==sorted(str2):
+            return True
+        return False
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = []
+        for i,s1 in enumerate(strs):
+            ana = [s1]
+            for j, s2 in enumerate(strs[i+1:]):
+                if self.checkAnagrams(s1,s2):
+                    ana.append(s2)
+                    strs.remove(s2)
+            anagrams.append(ana)
+        return anagrams
+
+s = Solution()
+strs = ["eat","tea","tan","ate","nat","bat"]
+s.groupAnagrams(strs)
